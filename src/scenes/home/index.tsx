177b6dc0e0ed953b956329df07593c3aec775857
tiagosousa <tiagosousa.tams@hotmail.com>
2025-02-12 20:16:18 +0000

    style complete and animations with motion

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { SelectedPage } from '@/shared/types';
 import ActionButton from '@/shared/ActionButton';
 import HomePageText from '@/assets/HomePageText.png'
@@ -8,7 +7,7 @@ import SponsorForbes from '@/assets/SponsorForbes.png'
 import SponsorFortune from '@/assets/SponsorFortune.png'
 import useMediaQuery from '@/hooks/useMediaQuery';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
-import { div } from 'framer-motion/client';
+import { motion } from 'framer-motion';
 
 
 type Props = {
@@ -25,23 +24,46 @@ const Home = ({setSelectedPage}: Props) => {
       className='gap-16 bg-gray-20 py-10 md:h-full md:pb-0'
     >
       {/*IMAGE AND MAIN HEADER */}
-      <div className='md:flex mx-auto w-5/6 items-center justify-center md:h-5/6'>
+      <motion.div 
+         className='md:flex mx-auto w-5/6 items-center justify-center md:h-5/6'
+         onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
+      >
          {/*MIAN HEADER */}
          <div className='z-10 mt-32 md:basis-3/5'>
          {/*HEADINGS */}
-         <div>
-            <div>
-               <div>
+         <motion.div 
+            className='md:-mt-20'
+            initial="hidden"
+            whileInView="visible"
+            viewport={{once:true, amount:0.5}}
+            transition={{duration:0.5}}
+            variants={{
+               hidden: {opacity:0, x:-50},
+               visible: {opacity:1, x:0},              
+            }}
+         >
+            <div className='relative'>
+               <div className='before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext'>
                   <img src={HomePageText} alt="home-page-text" />
                </div>
             </div>
 
-            <p >
+            <p className='mt-8 text-sm md:text-start'>
             Lorem ipsum dolor sit amet consectetur adipiscing elit, facilisi rhoncus nullam et odio integer curabitur est, sapien dictumst vulputate fusce pretium fermentum.
             </p>
-         </div>
+         </motion.div>
          {/*ACTIONS */}
-         <div>
+         <motion.div 
+            className='mt-8 flex items-center gap-8 '
+            initial="hidden"
+            whileInView="visible"
+            viewport={{once:true, amount:0.5}}
+            transition={{delay:0.2, duration:0.5}}
+            variants={{
+               hidden: {opacity:0, x:-50},
+               visible: {opacity:1, x:0},              
+            }}
+         >
             <ActionButton setSelectedPage={setSelectedPage}>               
                Join Now
             </ActionButton>
@@ -52,19 +74,20 @@ const Home = ({setSelectedPage}: Props) => {
             >
                <p>Learn More</p>
             </AnchorLink>
-         </div>
+         </motion.div>
          </div>
          {/*IMAGE */}
-         <div>
+         <div className='flex basis-3/5 justify-center md:z-10
+           md:ml-40 md:mt-16 md:justify-items-end '>
             <img src={HomePageGraphic} alt="home-pageGraphic" />
          </div>
-      </div>
+      </motion.div>
 
       {/*SPONSORS */}
       {isAboveMediumScreens && (
-         <div>
-            <div>
-               <div>
+         <div className='h-[150px] w-full bg-primary-100 py-10'>
+            <div className='mx-auto w-5/6'>
+               <div className='flex w-3/5 items-center justify-between'>
                   <img src={SponsorRedbull} alt="redbull-sponsor" />
                   <img src={SponsorForbes} alt="forbes-sponsor" />
                   <img src={SponsorFortune} alt="fortune-sponsor" />
