0fedc260f51ee28ea3f8c311014c9a0f46719f36
tiagosousa <tiagosousa.tams@hotmail.com>
2025-02-12 00:21:05 +0000

    enum created to manipulate navbar pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { useState } from "react"
 import Navbar from "./scenes/navbar"  
+import { SelectedPage } from "./shared/types"
+
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home) // starts with home from enum
 
   return (
     <div className="app bg-gray-20" >
-     <Navbar />
+     <Navbar 
+      selectedPage={selectedPage} 
+      setSelectedPage={setSelectedPage}
+     />
     </div>
   )
 }
--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,11 +1,16 @@
 import { useState } from "react"
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid"
 import Logo from "@/assets/Logo.png"
+import Link from "./Link"
+import { SelectedPage } from "@/shared/types"
 
 
-type Props = {}
+type Props = {
+   selectedPage: SelectedPage,
+   setSelectedPage: (value: SelectedPage) => void 
+}
 
-const Navbar = (props: Props) => {
+const Navbar = ({selectedPage, setSelectedPage} : Props) => {
    const flexBetween  = "flex items-center justify-between"
 
    return (
@@ -21,10 +26,10 @@ const Navbar = (props: Props) => {
                {/* RIGHT SIDE */}
                <div className={`${flexBetween} w-full`}>
                   <div className={`${flexBetween} gap-8 text-sm`}>
-                     <p>Home</p>
-                     <p>Benefits</p>
-                     <p>Our Classes</p>
-                     <p>Contact Us</p>
+                     <Link page="Home" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+                     <Link page="Benefits" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+                     <Link page="Our Classes" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+                     <Link page="Contact us" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
                   </div>
                   <div className={`${flexBetween} gap-8`}>
                      <p>Sign In</p>
