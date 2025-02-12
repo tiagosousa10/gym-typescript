6afeb4385fbb841ff3fda10962d691b1064abef4
tiagosousa <tiagosousa.tams@hotmail.com>
2025-02-12 18:48:19 +0000

    use handleScroll if is on top of page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Navbar from "./scenes/navbar"  
 import { SelectedPage } from "./shared/types"
 
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home) // starts with home from enum
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if(window.scrollY === 0) { // if at the top of the page
+        setIsTopOfPage(true)
+        setSelectedPage(SelectedPage.Home)
+      }
+
+      if(window.scrollY !== 0) {
+        setIsTopOfPage(false) // if not at the top of the page
+      }
+    }
+    window.addEventListener("scroll", handleScroll) // add the scroll event listener
+
+    return () => window.removeEventListener("scroll", handleScroll)
+
+  },[])
+
 
   return (
     <div className="app bg-gray-20" >
      <Navbar 
+      isTopOfPage={isTopOfPage}
       selectedPage={selectedPage} 
       setSelectedPage={setSelectedPage}
      />
