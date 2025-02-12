408898462f1f732e3491dbb372c502fe086621ee
tiagosousa <tiagosousa.tams@hotmail.com>
2025-02-12 18:40:26 +0000

    adding mobile menu modal

--- src/scenes/navbar/Link.tsx
+++ src/scenes/navbar/Link.tsx
@@ -5,11 +5,10 @@ type Props = {
    page: string,
    selectedPage: SelectedPage,
    setSelectedPage: (value: SelectedPage) => void 
-
 }
 
 const Link = ({page, selectedPage, setSelectedPage} : Props) => {
-   const lowerCasePage = page.toLowerCase().replace(/ /g, "") // remove spaces
+   const lowerCasePage = page.toLowerCase().replace(/ /g, "") as SelectedPage // remove spaces 
   return (
     <AnchorLink
       className={`${selectedPage === lowerCasePage ? "text-primary-500" : ""}
